16461cc80d5090b6b89d352084986eb65a14ed55
burakdurmus23 <burakdurmus23@gmail.com>
2023-10-20 23:36:00 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { ToastrService } from 'ngx-toastr';
+declare var $: any
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EcommerceClientNg';
+  // constructor(private toastr: ToastrService){
+  //   toastr.success("Hi", "Person");
+  // }
 }
